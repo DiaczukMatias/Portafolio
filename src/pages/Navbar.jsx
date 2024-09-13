06f8ad042f7e55d62b12beee3666ffd02489c042
DiaczukMatias <diaczukm@gmail.com>
2024-09-12 22:13:45 -0300

    responsive

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,12 +1,62 @@
-import React from "react";
+// import React from "react";
+// import styles from "../cssModules/Navbar.module.css";
+// import DownloadCV from "./DowlandCV";
+
+// const Navbar = ({ sections, handleRender }) => {
+//   return (
+//     <nav className={styles.navbar}>
+//       <div className={styles["navbar-left"]}>
+//         <ul>
+//           {sections.map((section) => (
+//             <React.Fragment key={section.id}>
+//               <li>
+//                 <a
+//                   href={`#${section.id}`}
+//                   onClick={(e) => {
+//                     e.preventDefault();
+//                     handleRender(section.id);
+//                   }}
+//                 >
+//                   {section.name}
+//                 </a>
+//               </li>
+//               <hr className={styles.separator} />
+//             </React.Fragment>
+//           ))}
+//           <div className={styles.CV}>
+//             <DownloadCV />
+//           </div>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+import React, { useState } from "react";
 import styles from "../cssModules/Navbar.module.css";
-import DownloadCV from "./DowlandCV";
+import DowlandCV from "./DowlandCV";
 
 const Navbar = ({ sections, handleRender }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles["navbar-left"]}>
-        <ul>
+        <span
+          className={`${styles["navbar-toggle"]} ${
+            isMenuOpen ? styles.active : ""
+          }`}
+          onClick={handleToggleMenu}
+        >
+          &#9776;
+        </span>
+        <ul className={isMenuOpen ? styles.active : ""}>
           {sections.map((section) => (
             <React.Fragment key={section.id}>
               <li>
@@ -15,6 +65,7 @@ const Navbar = ({ sections, handleRender }) => {
                   onClick={(e) => {
                     e.preventDefault();
                     handleRender(section.id);
+                    setIsMenuOpen(false); // Close menu on item click
                   }}
                 >
                   {section.name}
@@ -24,7 +75,7 @@ const Navbar = ({ sections, handleRender }) => {
             </React.Fragment>
           ))}
           <div className={styles.CV}>
-            <DownloadCV />
+            <DowlandCV />
           </div>
         </ul>
       </div>
